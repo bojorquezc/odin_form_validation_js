34fb0952d5fdb80d3e60eec3ded0d990a94c3e24
Conrado <42232126+bojorquezc@users.noreply.github.com>
2023-11-01 21:07:11 -0700

    Complete JS form validation exercise

--- validation.js
+++ validation.js
@@ -1,12 +1,14 @@
-// Objects for form elements and errors
+// OBJECTS FOR FORM ELEMENTS AND ERRORS
 const form = {
+    countryInput: document.getElementById('country_input'),
+    dialog: document.querySelector('.submit_dialog'),
+    dialogCloseButton: document.querySelector('.dialog_close_button'),
     element: document.getElementById('validation_form'),
     emailInput: document.getElementById('email_input'),
-    countryInput: document.getElementById('country_input'),
-    zipCodeInput: document.getElementById('zip_code_input'),
     originalPasswordInput: document.getElementById('original_password_input'),
     repeatPasswordInput: document.getElementById('repeat_password_input'),
     submitButton: document.querySelector('.submit_button'),
+    zipCodeInput: document.getElementById('zip_code_input'),
 }
 
 const error = {
@@ -23,20 +25,21 @@ const error = {
         span: document.querySelector('.zip_error'),
         1: 'Zip code can\'t be empty',
         2: 'Zip code needs to be 5 digits long',
+    },
+    originalPassword: {
+        span: document.querySelector('.original_password_error'),
+        1: 'Password can\'t be empty',
+        2: 'Password needs to be at least 8 characters long',
+    },
+    repeatPassword: {
+        span: document.querySelector('.repeat_password_error'),
+        1: 'Password can\'t be empty',
+        2: 'Passwords do not match!',
     }
 }
 
 
 // EMAIL ERRORS
-form.emailInput.addEventListener('input', (e) => {
-    if (form.emailInput.validity.valid) {
-        error.email.span.textContent = '';
-        error.email.span.className = 'error';
-    } else {
-        emailError();
-    }
-});
-
 function emailError () {
     if (form.emailInput.validity.valueMissing) {
         error.email.span.textContent = error.email[1];
@@ -46,7 +49,23 @@ function emailError () {
     error.email.span.className = 'error active';
 }
 
+form.emailInput.addEventListener('input', (e) => {
+    if (form.emailInput.validity.valid) {
+        error.email.span.textContent = '';
+        error.email.span.className = 'error';
+    } else {
+        emailError();
+    }
+});
+
 // COUNTRY ERRORS
+function countryError () {
+    if (form.countryInput.validity.valueMissing) {
+        error.country.span.textContent = error.country[1];
+    }
+    error.country.span.className = 'error active';
+}
+
 form.countryInput.addEventListener('input', (e) => {
     if (form.countryInput.validity.valid) {
         error.country.span.textContent = '';
@@ -56,14 +75,16 @@ form.countryInput.addEventListener('input', (e) => {
     }
 })
 
-function countryError () {
-    if (form.countryInput.validity.valueMissing) {
-        error.country.span.textContent = error.country[1];
+// ZIP CODE ERRORS
+function zipError () {
+    if (form.zipCodeInput.validity.valueMissing) {
+        error.zipCode.span.textContent = error.zipCode[1];
+    } else if (form.zipCodeInput.validity.tooShort) {
+        error.zipCode.span.textContent = error.zipCode[2];
     }
-    error.country.span.className = 'error active';
+    error.zipCode.span.className = 'error active';
 }
 
-// ZIP CODE ERRORS
 form.zipCodeInput.addEventListener('input', (e) => {
     if (form.zipCodeInput.validity.valid) {
         error.zipCode.span.textContent = '';
@@ -73,11 +94,60 @@ form.zipCodeInput.addEventListener('input', (e) => {
     }
 })
 
-function zipError () {
-    if (form.zipCodeInput.validity.valueMissing) {
-        error.zipCode.span.textContent = error.zipCode[1];
-    } else if (form.zipCodeInput.validity.tooShort) {
-        error.zipCode.span.textContent = error.zipCode[2];
+// ORIGINAL PASSWORD ERRORS
+function originalPasswordError () {
+    if (form.originalPasswordInput.validity.valueMissing) {
+        error.originalPassword.span.textContent = error.originalPassword[1];
+    } else if (form.originalPasswordInput.validity.tooShort) {
+        error.originalPassword.span.textContent = error.originalPassword[2];
     }
-    error.zipCode.span.className = 'error active';
+    error.originalPassword.span.className = 'error active';
 }
+
+form.originalPasswordInput.addEventListener('input', (e) => {
+    if (form.originalPasswordInput.validity.valid) {
+        error.originalPassword.span.textContent = '';
+        error.originalPassword.span.className = 'error';
+    } else {
+        originalPasswordError();
+    }
+})
+
+// REPEAT PASSWORD ERRORS
+function repeatPasswordError () {
+    if (form.repeatPasswordInput.validity.valueMissing) {
+        error.repeatPassword.span.textContent = error.repeatPassword[1];
+    } else if (form.repeatPasswordInput.value !== form.originalPasswordInput.value) {
+        error.repeatPassword.span.textContent = error.repeatPassword[2];
+    }
+    error.repeatPassword.span.className = 'error active';
+}
+
+form.repeatPasswordInput.addEventListener('input', (e) => {
+    if (form.repeatPasswordInput.validity.valid) {
+        error.repeatPassword.span.textContent = '';
+        error.repeatPassword.span.className = 'error';
+    } else {
+        repeatPasswordError();
+    }
+})
+
+// FORM SUBMIT
+form.element.addEventListener('submit', (e) => {
+    if (form.emailInput.validity.valid === false ||
+        form.countryInput.validity.valid === false ||
+        form.zipCodeInput.validity.valid === false ||
+        form.originalPasswordInput.validity.valid === false ||
+        form. repeatPasswordInput.validity.valid === false) {
+        e.preventDefault();
+    } else {
+        e.preventDefault();
+        form.element.reset();
+        form.dialog.showModal();
+    }
+})
+
+// FORM DIALOG CLOSE
+form.dialogCloseButton.addEventListener('click', () => {
+    form.dialog.close();
+})
